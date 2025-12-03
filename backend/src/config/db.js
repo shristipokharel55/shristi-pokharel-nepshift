59054ec4a458a9f12a5cc3236f5aeba4fbd227f9
shristipokharel55 <shristipokharel889@gmail.com>
2025-12-03 22:33:12 +0545

    Mongodb connected successfully

---
+++ backend/src/config/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      serverSelectionTimeoutMS: 5000,
+    });
+    console.log("MongoDB Connected Successfully");
+  } catch (error) {
+    console.log("MongoDB Connection Failed:", error.message);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
---
+++ backend/src/server.js
@@ -0,0 +1,24 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import connectDB from "./config/db.js";
+
+dotenv.config();           // Load environment variables
+connectDB();               // Connect MongoDB
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+// Test Route
+app.get("/", (req, res) => {
+  res.send("Nepshift Backend Running with ES Modules 🚀");
+});
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
